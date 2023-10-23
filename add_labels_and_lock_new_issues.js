dcf5c6bd875a908e5ff65c5a72fa58e471d67fcc
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-10-23 23:48:40 +0530

    chore: arranged code and added checks

--- add_labels_and_lock_new_issues.js
+++ add_labels_and_lock_new_issues.js
@@ -9,44 +9,53 @@ async function add_labels_and_lock_new_issues(octokit) {
         // get the issue
         const issue = Issue.getInstance();
 
-        // get the issue labels
-        const issue_labels_response = await octokit.rest.issues.listLabelsOnIssue({
-            owner: github.context.payload.repository.owner.login,
-            repo: github.context.payload.repository.name,
-            issue_number: issue.actions_payload.number,
-        });
-        if (issue_labels_response.status == 200) {
-            core.info("Issue labels fetched successfully");
-            const hasPendingTriageLabel = issue_labels_response.data.some(label => label.name === 'pending-triage');
-            const hasMaxAssigneesLabel = issue_labels_response.data.some(label => label.name === 'max-assignees-1');
+        if (github.context.eventName === "issue" && github.context.payload.action === "opened" && !issue?.isPullRequest()) {
+            // fetch the issue labels
+            const issue_labels = (await issue.details["labels"]) || [];
+            const hasPendingTriageLabel = issue_labels.some(label => label.name === 'pending-triage');
+            const hasMaxAssigneesLabel = issue_labels.some(label => label.name.match(/max-assignees-[0-9]{1,2}/));
+            // prepare the missing labels list
+            let missingLabels = [];
             if (!hasPendingTriageLabel) {
-                core.info("Issue does not have pending-triage label");
-                await issue.addLabels(['pending-triage']);
+                core.info("Issue does not have any label");
+                missingLabels.push('pending-triage');
             }
             if (!hasMaxAssigneesLabel) {
-                core.info("Issue does not have max-assignees-1 label");
-                await issue.addLabels(['max-assignees-1']);
+                core.info("Issue does not have max-assignees label");
+                // adding max-assignees-1 by default
+                missingLabels.push('max-assignees-1');
             }
-        } else {
-            core.setFailed("Failed to fetch issue labels");
-            return;
-        }
-        // lock the conversation to author and org members unless approved by triage team
-        const lockResponse = await octokit.rest.issues.lock(
-            {
+            // add the missing labels
+            const add_labels_reponse = await octokit.rest.issues.addLabels({
                 owner: github.context.payload.repository.owner.login,
                 repo: github.context.payload.repository.name,
                 issue_number: issue.actions_payload.number,
-                lock_reason: 'spam',
+                labels: missingLabels,
+            });
+
+            if (add_labels_reponse.status == 200) {
+                core.info("Labels added successfully");
+            } else {
+                core.setFailed("Failed to add labels");
+                return;
             }
-        );
-        if (lockResponse.status == 204) {
-            core.info("Issue locked successfully");
-        } else {
-            core.setFailed("Failed to lock issue");
+
+            // lock the conversation to author and org members unless approved by triage team
+            const lockResponse = await octokit.rest.issues.lock(
+                {
+                    owner: github.context.payload.repository.owner.login,
+                    repo: github.context.payload.repository.name,
+                    issue_number: issue.actions_payload.number,
+                }
+            );
+            if (lockResponse.status == 204) {
+                core.info("Issue locked successfully");
+            } else {
+                core.setFailed("Failed to lock issue");
+            }
         }
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed(error?.message);
     }
 }
 
--- helpers.js
+++ helpers.js
@@ -2,10 +2,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 // This function will verify action has been performed by a member of triage team
-function verifyTriageTeam(){
+function verifyTriageTeam() {
     const triageTeamUsernames = core.getInput("triage-team-usernames").split(",");
     const actor = github.context.actor;
     return triageTeamUsernames.includes(actor);
 }
 
-module.exports = { verifyTriageTeam };
+module.exports = { verifyTriageTeam, addLabels };
