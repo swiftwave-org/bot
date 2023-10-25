ee6d064e3eef4c39f47e3d967586b7cabda140f7
Tanmoy <ts741127@gmail.com>
2023-10-25 23:37:54 +0530

    chore: file renamed

--- dist/index.js.cache.js
+++ dist/index.js.cache.js
@@ -262,74 +262,6 @@ module.exports = act_on_update_command
 
 /***/ }),
 
-/***/ 8630:
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-const core = __webpack_require__(2186);
-const github = __webpack_require__(5438);
-const { Issue } = __webpack_require__(9882);
-
-
-
-async function add_labels_and_lock_new_issues(octokit) {
-    try {
-        // get the issue
-        const issue = await Issue.getInstance();
-
-        if (github.context.eventName === "issues" && github.context.payload.action === "opened" && !issue.isPullRequest()) {
-            // fetch the issue labels
-            const issue_labels = issue.details.labels;
-            const hasPendingTriageLabel = issue_labels.some(label => label.name === 'pending-triage');
-            const hasMaxAssigneesLabel = issue_labels.some(label => label.name.match(/max-assignees-[0-9]{1,2}/));
-            // prepare the missing labels list
-            let missingLabels = [];
-            if (!hasPendingTriageLabel) {
-                core.info("Issue does not have any label");
-                missingLabels.push('pending-triage');
-            }
-            if (!hasMaxAssigneesLabel) {
-                core.info("Issue does not have max-assignees label");
-                // adding max-assignees-1 by default
-                missingLabels.push('max-assignees-1');
-            }
-            // add the missing labels
-            const add_labels_reponse = await octokit.rest.issues.addLabels({
-                owner: github.context.payload.repository.owner.login,
-                repo: github.context.payload.repository.name,
-                issue_number: issue.actions_payload.number,
-                labels: missingLabels,
-            });
-
-            if (add_labels_reponse.status == 200) {
-                core.info("Labels added successfully");
-            } else {
-                core.setFailed("Failed to add labels");
-                return;
-            }
-
-            // lock the conversation to author and org members unless approved by triage team
-            const lockResponse = await octokit.rest.issues.lock(
-                {
-                    owner: github.context.payload.repository.owner.login,
-                    repo: github.context.payload.repository.name,
-                    issue_number: issue.actions_payload.number,
-                }
-            );
-            if (lockResponse.status == 204) {
-                core.info("Issue locked successfully");
-            } else {
-                core.setFailed("Failed to lock issue");
-            }
-        }
-    } catch (error) {
-        core.setFailed(error?.message);
-    }
-}
-
-module.exports = add_labels_and_lock_new_issues
-
-/***/ }),
-
 /***/ 693:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -30207,6 +30139,14 @@ module.exports = { Issue, IssueComment };
 
 /***/ }),
 
+/***/ 7104:
+/***/ ((module) => {
+
+module.exports = eval("require")("./add_labels_and_lock_new_issues");
+
+
+/***/ }),
+
 /***/ 9491:
 /***/ ((module) => {
 
@@ -30478,7 +30418,7 @@ const github = __webpack_require__(5438);
 const act_on_pending_triage_removal = __webpack_require__(3834);
 const act_on_approve_command = __webpack_require__(8319);
 const act_on_update_command = __webpack_require__(9468);
-const add_labels_and_lock_new_issues = __webpack_require__(8630);
+const add_labels_and_lock_new_issues = __webpack_require__(7104);
 
 const run = async () => {
     const token = core.getInput('token', { required: true });
